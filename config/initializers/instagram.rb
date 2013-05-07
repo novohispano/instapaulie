80b5aa80059514b7d5e60f83c3f19a04a60f5124
novohispano@gmail.com <novohispano@gmail.com>
2013-05-07 11:05:22 -0600

    First commit

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,6 @@
+require "instagram"
+
+Instagram.configure do |config|
+  config.client_id = "9fd86727b30b4865977c709eff090b47"
+  config.client_secret = "b005f1f4eb1148d9a8b49aca405aaf00"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instagram::Application.config.secret_token = 'eff5bed36cd001ce93b282ff93f2423edc009a8b529226f73464fedcb5c67fae0446d665f57d761fed4ac3e21f8ed10eaeda86f38eb40e8b4e4146d2f7109062'
