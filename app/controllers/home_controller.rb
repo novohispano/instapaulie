d3c16d0f79934a3fa924be380ab9cd8d83d8cdfa
novohispano@gmail.com <novohispano@gmail.com>
2013-05-07 11:41:13 -0600

    Works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @instagram = Instagram.user_recent_media(20784237, {:count => 1})
+    @instagram = Instagram.user_recent_media(20784237, {:count => 10})
   end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -3,4 +3,6 @@ require "instagram"
 Instagram.configure do |config|
   config.client_id = "9fd86727b30b4865977c709eff090b47"
   config.client_secret = "b005f1f4eb1148d9a8b49aca405aaf00"
+  config.access_token = "20784237.9fd8672.f1374fae4d6a4c16bc28b5ddeef3a312"
+
 end
